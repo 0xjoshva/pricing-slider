387577d3739911d64741029e81b633d05f006792
0xjoshva <joshuasteed123@gmail.com>
2022-10-27 16:25:54 +0200

    new push

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@600;800&display=swap");
+/* font-family: 'Manrope', sans-serif; */
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+html,
+body {
+  height: 100%;
+  font-size: 15px;
+}
+body {
+  line-height: 1.5;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+img,
+picture,
+video,
+canvas,
+svg {
+  display: block;
+  max-width: 100%;
+}
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+}
+#root,
+#__next {
+  isolation: isolate;
 }
 
-nav {
-  padding: 30px;
-}
+:root {
+  /* primary */
+   --softcyan: hsl(174, 77%, 80%); /*Full Slider Bar */
+  --strongcyan: hsl(174, 86%, 45%); /*Slider Background */
+  --greyred: hsl(14, 92%, 95%); /*Discount Background */
+  --lightred: hsl(15, 100%, 70%); /*Discount Text */
+  --paleblue: hsl(226, 100%, 87%); /*CTA Text */
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  /* neutral */
+  --white: hsl(0, 0%, 100%); /*Pricing Component Background */
+  --verypaleblue: hsl(230, 100%, 99%); /*Main Background*/
+  --lightgreyishblue1: hsl(224, 65%, 95%); /*Empty Slider Bar*/
+  --lightgreyishblue2: hsl(223, 50%, 87%); /*Toggle Background*/
+--greyishblue: hsl(225, 20%, 60%); /*Text*/
+--darkblue: hsl(227, 35%, 25%); /*Text & CTA Background*/
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,201 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<section>
+  <div id="heading">
+  <h1>Simple, traffic-based pricing</h1>
+   <p>Sign-up for our 30-day trial. No credit card required.</p>
+   </div>
+  <div class="card">
+  <div class="top">
+    <p id="views">100K PAGEVIEWS</p>
+    <p id="price">$16.00 <span>/ month</span></p>
   </div>
+<div class="rangediv">
+    <input type="range" min="1" max="100" value="50" id="rangeinput">
+    </div>
+  <div class="range">
+      <p class="period">Monthly Billing</p>
+      
+      <label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
+      <p class="period year">Yearly Billing</p>
+    </div>
+    <div class="container-end">
+      <ul>
+        <li> <img src="../assets/icon-check.svg" alt=""> <p>Unlimited websites</p></li>
+        <li><img src="../assets/icon-check.svg" alt=""> <p>100% data ownership</p></li>
+        <li> <img src="../assets/icon-check.svg" alt=""> <p>Email reports</p></li>
+      </ul>
+      <button>Start my trial</button>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+<style scoped>
+section{
+  background-image: url(../assets/bg-pattern.svg);
+  background-color: var(--verypaleblue);
+  width: 100%;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: 100vw 50vh;
+  display: flex;
+  flex-direction: column;
+ justify-content: flex-start;
+ align-items: center;
+ font-family: 'Manrope', sans-serif; 
+}
+#heading{
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background-image: url(../assets/pattern-circles.svg);
+background-repeat: no-repeat;
+background-position: center;
+width: fit-content;
+height: 12rem;
+margin-bottom: 3rem;
+margin-top: 3rem;
+}
+#heading h1{
+  font-size: 2.2rem;
+  color: var(--darkblue);
+}
+#heading p{
+  color: var(--greyishblue);
+  font-size: 1.1rem;
+  padding-top: .5rem;
+}
+.card{
+  width: 45rem;
+  height: 33rem;
+  background: var(--white);
+  box-shadow: 0px 5px 30px 2px var(--lightgreyishblue1);
+  border-radius: 12px;
+  padding: 3.5rem;
+}
+.top{
+
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.top #views{
+  letter-spacing: 1px;
+  font-size: 1.1rem;
+  color: var(--greyishblue);
+}
+#price{
+  font-size:2.4rem;
+  font-weight: 800;
+  color: var(--darkblue);
+}
+#price span{
+  font-size: 1.2rem;
+  font-weight: 600;
+  color: var(--greyishblue);
+  vertical-align: middle;
+}
+.switch {
+  position: relative;
+  display: inline-block;
+  min-width: 60px;
+  min-height: 34px;
+}
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s all ease-in-out;
+}
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+input:checked + .slider {
+ background:linear-gradient(180deg, hsl(236, 72%, 79%), hsl(237, 63%, 64%));
+}
+input:focus + .slider {
+  box-shadow: 0 0 1px hsl(237, 63%, 64%);
+}
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+.slider.round:before {
+  border-radius: 50%;
+}
+.range{
+  display: flex;
+  width: fit-content;
+  height: fit-content;
+  align-items: center;
+  justify-content: center;
+  column-gap: 1.5rem;
+  width: 100%;
+}
+.year::after{
+content: "25% discount";
+font-size: small;
+padding: .4rem .6rem;
+border-radius: 26px;
+background-color: var();
+}
+.range p{
+    color: var(--lightgreyblue) !important;
+}
+li{
+  list-style-type: none;
+  line-height: 2rem;
+  display: flex;
+  align-items: center;
+  column-gap: 1.4rem;
+}
+li img{
+  width: 1rem;
+  height: auto;
+}
+.container-end{
+  display: flex;
+
+}
+.rangediv{
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+#rangeinput{
+  width: 100%;
+
+}
+</style>
